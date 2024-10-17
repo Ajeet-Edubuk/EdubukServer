5c36bd9f45f80c613dbec7dd07229e4e392b5d63
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-10-17 17:45:16 +0530

    redesign schema and added share and remove functionality

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {getRecordByURI, getUser, removeAccess, shareAccess} from '../controller/shareAccess.js'
+import {deleteUser, getDocByUri, getRecordByURI, getUser, removeAccess, shareAccess} from '../controller/shareAccess.js'
 
 const router = express.Router();
 
 router.post('/shareAccess',shareAccess);
 router.get('/getResponse/:userId',getUser);
 router.put('/removeAccess',removeAccess);
-router.get('/recordByUri',getRecordByURI);
+router.get('/recordByUri/:pinataHash',getRecordByURI);
+router.get('/getDocByUri/:pinataHash',getDocByUri);
+router.delete('/deleteUser',deleteUser);
 
 export default router;
